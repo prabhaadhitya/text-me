b4cf9c6d3450f86ef95fe0e7c4d4b6d2577371f6
Prabha Adhitya <prabhaadhitya01@gmail.com>
2026-02-03 13:34:00 +0530

    Ready to deploy

---
+++ backend/src/routes/message.route.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+
+import { getAllContacts, getChatPartners, getMessagesByUserId, sendMessage } from "../controllers/message.controller.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+
+const router = Router(); 
+
+router.use(arcjetProtection, protectRoute);
+
+router.get('/contacts', getAllContacts);
+router.get('/chats', getChatPartners);
+router.get('/:id', getMessagesByUserId);
+router.post('/send/:id', sendMessage);
+
+export default router;
---
+++ backend/src/server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import path from "path";
+import cors from 'cors';
+
+import { ENV } from './lib/env.js';
+import { connectDB } from './lib/db.js';
+import { app, server } from "./lib/socket.js";
+import authRoute from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
+
+const __dirname = path.resolve();
+const PORT = ENV.PORT || 3000;
+
+app.use(express.json({ limit: "5mb" }));
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(cookieParser());
+
+app.use('/api/auth/', authRoute);
+app.use("/api/messages", messageRoutes);
+
+if (ENV.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (_, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
+
+server.listen(PORT, () => {
+    connectDB();
+    console.log(`Server running on port ${PORT}`)
+})
