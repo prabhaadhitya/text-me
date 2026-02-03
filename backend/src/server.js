cbf595b0d4687b7cdc75a2b1e325f1bd12ad7dc3
Prabha Adhitya <prabhaadhitya01@gmail.com>
2026-02-03 14:54:16 +0530

    SPA fallback for Node 22 using app.use

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,7 @@ import authRoute from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 
 const __dirname = path.resolve();
-const PORT = ENV.PORT || 3000;
+const PORT = ENV.PORT || 10000;
 
 app.use(express.json({ limit: "5mb" }));
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
@@ -19,13 +19,15 @@ app.use(cookieParser());
 app.use('/api/auth/', authRoute);
 app.use("/api/messages", messageRoutes);
 
-if (ENV.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.get("/api/health", (req, res) => {
+  res.json({ status: "OK" })
+})
 
-    app.get("/:path(*)", (_, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    });
-}
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+})
 
 server.listen(PORT, () => {
     connectDB();
